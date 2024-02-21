62e725f9932fae2d8ad756136a57b7991dadafc4
Besos4Eddie <157508332+Besos4Eddie@users.noreply.github.com>
2024-02-21 11:44:38 -0600

    Create marketing.kt

---
+++ marketing.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+   val extrovert = "450"
+   val introvert = "20"
+   if (extrovert < introvert ) {
+       println("You prefer dogs?")
+   } else {
+       println("Are you a cat person?")
+   }
+}
+
